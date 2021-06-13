32029441cbd02849a9bc8378353c0100c88209c3
mstrcoder <ifanshakil15@gmail.com>
2021-06-13 18:30:29 +0530

    Created the home.ejs page for My Website

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app/app');
+const express = require('express');
+const ejs = require('ejs');
+const expressLayout=require('express-ejs-layouts'); 
+const path = require('path');
+const app=express();
 
+// app.set('view engine','pug')
+// app.set('views',path.join(__dirname,'views'))
+//Assets we use to use the
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs');
 const PORT=process.env.PORT||5000;
 
 app.listen(PORT, ()=>{
